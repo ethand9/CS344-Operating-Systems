8419c23a95b637df6b3fbcdc75c909c408f97340
ethand9 <43592552+ethand9@users.noreply.github.com>
2019-11-08 21:24:36 -0800

    cleaned up code

--- hw2/duonget.buildrooms.c
+++ hw2/duonget.buildrooms.c
@@ -13,15 +13,9 @@ enum roomType{ //enum of room types
     START, END, MID
 };
 
-enum roomNames{ //enum of all rooms
-    CAVERN, LAKE, TUNNEL, MINE, DUNGEON,
-    ALCOVE, RUINS, SINKHOLE, HIDEOUT, RUBBLE
-};
-
 struct room{
     char*roomName; //Room name
     enum roomType type; //Type of room
-    // struct room*connections[CONNECT_TOTAL]; //paths to other rooms
     int connections[CONNECT_TOTAL]; //paths to other rooms
     int connectionsNum; //number of connections this room has
     int chosen; //bool if a room has been selected to be built or not
@@ -35,77 +29,17 @@ void choose(struct room*room); //randomly choose the rooms to build
 void connect(struct room*room); //randomly assign room types and randomly connect the rooms to each other
 int isConnectionsMet(struct room*room); //checks if each built room has at least 3 connections
 void makeDirectory(struct room*room); //make a folder and put the room files in it
-// struct room initRoom(struct room*room);
 
 main(){
     srand(time(0)); // randomize the seed
 
-    printf("%s\n", "Hello world");
-    int i, j;
     struct room rooms[ROOM_TOTAL];
-    // struct room cave;
-    // struct room lake;
-    // struct room tunnel;
 
     nameRooms(rooms); //name the rooms 
     clearConnections(rooms); //set all connections to null
     choose(rooms); //randomly choose the rooms to build
     connect(rooms); //randomly connect the rooms
-    makeDirectory(rooms);
-
-    // for(i=0; i<ROOM_TOTAL; i++){
-    //     if(rooms[i].chosen == 1){
-    //         printf("%d\n", rooms[i].type);
-    //     }
-    // }
-
-    // for(i=0; i<ROOM_TOTAL; i++){
-    //     for(j=0; j<ROOM_MAX; j++){
-    //         printf("%s%d%s%d\n", "i: ", i, "j: ", j);
-    //     }
-    // }
-
-
-    // for(i=0; i<ROOM_TOTAL; i++){
-    //     for(j=0; j<ROOM_MAX; j++){
-    //         // printf("%s%d%s%d\n", "i: ", i, "j: ", j);
-    //         // if(rooms[i].connections[j]){
-    //         if(rooms[i].chosen == 0){
-    //             printf("%s\n", "true");
-    //             // printf("%s\n", "here2");
-    //         }
-    //         else{
-    //             printf("%s\n", "false");
-    //             // printf("%s\n", "here3");
-    //         }
-    //     }
-    // }
-    // printf("%s\n", "here1");
-    
-    // return 0;
-
-    // tunnel = initRoom(&tunnel);
-    // printf("%s\n", tunnel.roomName);
-    // rooms[0] = tunnel;
-    // printf("%s\n", rooms[0].roomName);
-
-    // cave.roomName = "test1234";
-    // printf("%s\n", cave.roomName);
-    // cave.type = MID;
-    // printf("%d\n", cave.type);
-
-    // lake.roomName = "lake";
-    // cave.ptr[0] = &lake;
-    // cave.ptr[1] = NULL;
-    // printf("%s\n", cave.ptr[0]->roomName);
-
-    // if(cave.ptr[0]){
-    //     printf("%s\n", "true");
-    //     // printf("%s\n", cave.ptr[0]);
-    // }
-    // else{
-    //     printf("%s\n", "false");
-    // }
+    makeDirectory(rooms); //create the folder with the files and their contents
 
 }
 
@@ -123,7 +57,7 @@ void nameRooms(struct room*room){
     room[9].roomName = "rubble";
 }
 
-//set all connections to null
+//reset all connections
 void clearConnections(struct room*room){
     int i, j;
     for(i=0; i<ROOM_TOTAL; i++){ //loop for all rooms
@@ -148,24 +82,16 @@ void choose(struct room*room){
     while(num < ROOM_MAX){ //build seven rooms
         elem = rand()%ROOM_TOTAL; //randomly choose a room to build
         if(room[elem].chosen != 1){ //check that the room is not already built
-            room[elem].chosen = 1;
+            room[elem].chosen = 1; //indicate that the room is built
             num++;
         }
     }
-
-    // for(i=0; i<ROOM_TOTAL; i++){
-    //     if(room[i].chosen == 1){
-    //         printf("%d", i);
-    //     }
-    // }
 }
 
 //randomly connect the rooms together
 void connect(struct room*room){
-    int i, j, k, elem, elem2, num, count1, count2;
+    int i, elem, elem2;
     int flag1 = 1;
-    int connections = 0;
-    int connected = 0;
 
     //set all rooms types to MID
     for(i=0; i<ROOM_TOTAL; i++){
@@ -192,81 +118,6 @@ void connect(struct room*room){
             flag1 = 0;
         }
     }
-
-    //randomly connect the rooms together
-    // for(i=0; i<ROOM_TOTAL; i++){
-    //     if(room[i].chosen == 1){
-    //         num = ((rand()%4) + 3);
-    //         while(connected < ROOM_TOTAL){
-    //             num = rand()%CONNECT_TOTAL;
-    //             if(room[i].connections[num]){
-    //                 //figure out how to do connections without overlapping
-    //             }
-    //             connected++;
-    //         }
-    //     }
-    // }
-
-
-    // for(i=0; i<ROOM_TOTAL; i++){ //loop for all rooms
-    //     for(j=0; j<CONNECT_TOTAL; j++){ //loop for all connections
-    //         printf("%d\n", room[i].connections[j]);
-    //     }
-    // }
-
-
-    // //loop for seven rooms
-    // do{
-    //     // printf("%s\n", "here0");
-    //     //randomly choose a room
-    //     elem = rand()%ROOM_TOTAL;
-    //     //check if it is a built room
-    //     //check if it has connections already
-    //     if((room[elem].chosen == 1) && (room[elem].connected != 1)){
-    //         room[elem].connected = 1;
-    //         //if not then randomly choose how many connections it will have between 3 to 6
-    //         num = ((rand()%4) + 3);
-    //         count1 = -1;
-    //             do{
-    //                 count1++;
-    //                 count2 = room[elem].connections[count1];
-    //             }while(count2 != -1);
-    //             connections = count1;
-    //         // printf("%d\n", num);
-    //         //randomly choose those connections
-    //         while(connections < num){
-    //             // printf("%s\n", "here000");
-    //             elem2 = rand()%ROOM_TOTAL;
-    //             if((room[elem2].chosen == 1) && (room[elem2].picked != 1) && (elem != elem2)){
-    //                 room[elem2].picked = 1;
-    //                 count1 = -1;
-    //                 do{
-    //                     count1++;
-    //                     count2 = room[elem].connections[count1];
-    //                 }while(count2 != -1);
-    //                 room[elem].connections[count2] = elem2;
-
-    //                 count1 = -1;
-    //                 do{
-    //                     count1++;
-    //                     count2 = room[elem].connections[count1];
-    //                 }while(count2 != -1);
-    //                 room[elem2].connections[count2] = elem;
-
-
-    //                 connections++;
-    //             }
-    //         }
-    //         // connections = 0;
-    //         //clear the picked bools
-    //         for(i=0; i<ROOM_TOTAL; i++){
-    //             room[i].picked = 0;
-    //         }
-    //         connected++;
-    //         // printf("%s\n", "here1");
-    //     }
-    //     // printf("%s\n", "here2");
-    // }while(connected < ROOM_MAX);
 
     do{ //loop until connections requirement is met
         //randomly choose a room
@@ -295,24 +146,13 @@ void connect(struct room*room){
             }
         }
     }while(isConnectionsMet(room) == 0);
-
-
-
-
-    // for(i=0; i<ROOM_TOTAL; i++){ //loop for all rooms
-    //     for(j=0; j<CONNECT_TOTAL; j++){ //loop for all connections
-    //         printf("%d\n", room[i].connections[j]);
-    //     }
-    // }
-    
-
 }
 
 int isConnectionsMet(struct room*room){ //checks if each built room has at least three connections
     int i;
     int met = 1;
     for(i = 0; i < ROOM_TOTAL; i++){
-        if((room[i].chosen == 1) && (room[i].connectionsNum < 3)){
+        if((room[i].chosen == 1) && (room[i].connectionsNum < 3)){ //conditions not met
             met = 0;
             break;
         }
@@ -329,14 +169,16 @@ void makeDirectory(struct room*room){ //create a directory and store the room fi
     sprintf(folderName, "duonget.rooms.%d", pid); //create the folder name
     mkdir(folderName); //create the folder
     chdir(folderName); //go to the folder
+
     //create the files and their contents
     for(i = 0; i < ROOM_TOTAL; i++){ //loop for all rooms
         if(room[i].chosen == 1){ //only write the built rooms
             roomFile = fopen(room[i].roomName, "a"); //create a file with the room name
+
             //write the file's contents
             //write the room name
             fprintf(roomFile, "%s%s%s", "ROOM NAME: ", room[i].roomName, "\n");
-            // fprintf(roomFile, room[i].roomName, "\n");
+
             //write the connections
             for(j = 0; j < CONNECT_TOTAL; j++){
                 if(room[i].connections[j] == -1){
@@ -344,6 +186,8 @@ void makeDirectory(struct room*room){ //create a directory and store the room fi
                 }
                 fprintf(roomFile, "%s%d%s%s%s", "CONNECTION ", j+1, ": ", room[room[i].connections[j]].roomName, "\n");
             }
+
+            //write the room type
             fprintf(roomFile, "%s", "ROOM TYPE: ");
             if(room[i].type == 0){
                 fprintf(roomFile, "START_ROOM\n");
@@ -357,6 +201,7 @@ void makeDirectory(struct room*room){ //create a directory and store the room fi
             else{
                 fprintf(roomFile, "ERROR\n");
             }
+
             fclose(roomFile); //close the file
         }        
     }
