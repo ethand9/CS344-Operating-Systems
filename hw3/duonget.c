66c8a9295a99cb439f48ec5b5b21abb5243f28b3
ethand9 <43592552+ethand9@users.noreply.github.com>
2019-11-20 16:40:52 -0800

    uploaded initial code

---
+++ hw3/duonget.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char* getInput();
+void doExit();
+void doCD();
+void doStatus();
+void doExec();
+
+int main(){
+    // printf("hello world\n");
+    // fflush(stdin);
+    char* userInput;
+    userInput = getInput();
+    printf("%s%s\n", "output: ", userInput);
+    if(strcmp(userInput, "exit\n") == 0){
+        doExit();
+    }
+    else if(userInput == "cd\n"){
+        doCD();
+    }
+    else if(userInput == "status\n"){
+        doStatus();
+    }
+    else{
+        
+        if(){
+
+        }
+        else{
+            doExec();
+        }
+    }
+}
+
+char* getInput(){
+    static char str[100];
+    printf("%s", ": ");
+    fgets(str, 100, stdin);
+    return str;
+}
+
+//exit
+void doExit(){
+    printf("exit\n");
+}
+
+//cd
+void doCD(){
+    printf("cd\n");
+}
+
+//status
+void doStatus(){
+    printf("status\n");
+}
+
+//exec
+void doExec(){
+    printf("exec\n");
+}
