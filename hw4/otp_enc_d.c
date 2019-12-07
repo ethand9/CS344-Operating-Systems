8faf95d22f68d8a70073aeeaf81344bc5d2b89d7
ethand9 <43592552+ethand9@users.noreply.github.com>
2019-12-06 23:15:33 -0800

    enc works

--- hw4/otp_enc_d.c
+++ hw4/otp_enc_d.c
@@ -13,7 +13,7 @@
 
 int main(int argc, char*argv[]){
     int i, portNumber, establishedConnectionFD, listenSocketFD, charsWritten, charsRead;
-    int plaintextFile, plaintextFileSize, keyFile, keyFileSize, msgLetter, keyLetter, cipher;
+    int plaintextFile, plaintextFileSize, keyFile, keyFileSize, textLetter, keyLetter, encMsg;
     int socketFD = socket(AF_INET, SOCK_STREAM, 0); //create socket
     struct sockaddr_in serverAddress, clientAddress;
     socklen_t sizeOfClientInfo;
@@ -26,14 +26,14 @@ int main(int argc, char*argv[]){
         exit(1);
     }
 
-    //convert num to int
-    portNumber = atoi(argv[1]);
-
     //socket() failed
     if(socketFD == -1){
         perror("error: socket() failed");
         exit(1);
     }
+
+    //convert num to int
+    portNumber = atoi(argv[1]);
 
     //clear out buffers
     memset(textBuffer, '\0', sizeof(textBuffer));
@@ -42,27 +42,26 @@ int main(int argc, char*argv[]){
     memset((char*)&serverAddress, '\0', sizeof(serverAddress)); //clear address struct
 
     //set up server
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
     serverAddress.sin_family = AF_INET;
     serverAddress.sin_port = htons(portNumber);
-    serverAddress.sin_addr.s_addr = INADDR_ANY;
 
+    //bind the socket to the given port
     if(bind(socketFD, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) == -1){
         perror("error: bind() failed");
         exit(2);
     }
 
-    if(listen(socketFD, MAX_QUEUE) == -1){
-        perror("error: listen() failed");
-        exit(2);
-    }
-
-    sizeOfClientInfo = sizeof(clientAddress);
+    //listen for other connections
+    listen(socketFD, MAX_QUEUE);
     while(1 == 1){
+        sizeOfClientInfo = sizeof(clientAddress);
         establishedConnectionFD = accept(socketFD, (struct sockaddr *)&clientAddress, &sizeOfClientInfo);
         if(establishedConnectionFD == -1){
             perror("error: accept() failed");
         }
 
+        //fork new connections
         spawnPid = fork();
         switch(spawnPid){
             case -1: //error
@@ -71,105 +70,54 @@ int main(int argc, char*argv[]){
                 break;
             case 0: //encryption
                 memset(textBuffer, 0, sizeof(textBuffer)); //clear buffer
-
-                //read text from client
-                plaintextFileSize = read(establishedConnectionFD, textBuffer, MAX_BUFFER);
-                // plaintextFileSize = lseek(plaintextFile, 0 , SEEK_END);
-                //read() failed
-                if(plaintextFileSize == -1){
+                
+                plaintextFileSize = read(establishedConnectionFD, textBuffer, MAX_BUFFER); //read text from client
+                if(plaintextFileSize == -1){ //read() failed
+                    perror("error: read() failed");
+                    exit(2);
+                }
+                
+                memset(keyBuffer, 0, sizeof(keyBuffer)); //clear buffer
+                
+                keyFileSize = read(establishedConnectionFD, keyBuffer, MAX_BUFFER); //read keyLetter from client
+                if(keyFileSize == -1){ //read() failed
                     perror("error: read() failed");
                     exit(2);
                 }
 
-                // //write ack to client
-                // charsWritten = write(establishedConnectionFD, "!", 1);
-                // if(charsWritten == -1){
-                //     perror("error: write() failed");
-                //     exit(2);
-                // }
-
-                //clear buffer
-                memset(keyBuffer, 0, sizeof(keyBuffer));
-
-                //read key from client
-                keyFileSize = read(establishedConnectionFD, keyBuffer, MAX_BUFFER);
-                // keyFileSize = lseek(keyFile, 0, SEEK_END);
-                //read() failed
-                if(keyFileSize == -1){
-                    perror("error: read() failed");
-                    exit(2);
-                }
-
-                //validate key
-
-                //validate plaintext
-
-                //encryption
+                //encrypt the message
                 for(i = 0; i < plaintextFileSize; i++){
                     //if character is a space change it to [
                     //then change value to ascii
-                    if(keyBuffer[i] != ' ')
+                    //subtract 65 to change range to 27 characters
+                    if(keyBuffer[i] != ' '){
                         keyLetter = (int)keyBuffer[i];
+                        keyLetter -= 65;
+                    }
                     else{
                         keyBuffer[i] = '[';
                         keyLetter = (int)keyBuffer[i];
+                        keyLetter -= 65;
                     }
-                    if(textBuffer[i] != ' ')
-                        msgLetter = (int)textBuffer[i];
+                    if(textBuffer[i] != ' '){
+                        textLetter = (int)textBuffer[i];
+                        textLetter -= 65;
+                    }
                     else{
                         textBuffer[i] = '[';
-                        msgLetter = (int)textBuffer[i];
+                        textLetter = (int)textBuffer[i];
+                        textLetter -= 65;
                     }
-                    //subtract 65 to change range to 27 characters
-                    keyLetter -= 65;
-                    msgLetter -= 65;
-    
-                    cipher = (keyLetter + msgLetter) % 27; //modular addition
-                    cipher += 65; //revert range
-                    encBuffer[i] = (char)cipher; //change back to letters
 
-                    if(encBuffer[i] == '[')
+                    encMsg = (keyLetter + textLetter) % 27; //modular addition
+                    encMsg += 65; //revert range
+                    encBuffer[i] = (char)encMsg; //change back to letters
+
+                    if(encBuffer[i] == '[') //change back to space
                         encBuffer[i] = ' ';
-
-                    // // change spaces to asterisks
-                    // if(textBuffer[i] == ' '){
-                    //     textBuffer[i] = '@';
-                    // }
-                    // if(keyBuffer[i] == ' '){
-                    //     keyBuffer[i] = '@';
-                    // }
-
-                    // // type conversion to int
-                    // msgLetter = (int)buffer1[i];
-                    // keyLetter = (int)buffer2[i];
-
-                    // // subtract 64 so that range is 0 - 26 (27 characters)
-                    // msgLetter = msgLetter - 64;
-                    // keyLetter = keyLetter - 64;
-
-                    // // combine key and message using modular addition
-                    // cipher = (msgLetter + keyLetter) % 27;
-
-                    // // add 64 back to that range is 64 - 90
-                    // cipher = cipher + 64;
-
-                    // // type conversion back to char
-                    // encBuffer[i] = (char)cipher + 0;
-
-                    // // after encryption, change asterisks to spaces
-                    // if(encBuffer[i] == '@'){
-                    //     encBuffer[i] = ' ';
-                    // }
                 }
-
                 //write encryption to client
                 charsWritten = write(establishedConnectionFD, encBuffer, plaintextFileSize);
-                //not all characters sent
-                if(charsWritten < plaintextFileSize){
-                    perror("error: write() failed");
-                    exit(2);
-                }
-
                 break;
             default: //parent
                 ; //pass
