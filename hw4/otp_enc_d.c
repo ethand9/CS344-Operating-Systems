219a2b169f59efbb381d3444eebd83fbae160321
ethand9 <43592552+ethand9@users.noreply.github.com>
2019-12-06 19:59:02 -0800

    cleaned up code

--- hw4/otp_enc_d.c
+++ hw4/otp_enc_d.c
@@ -41,11 +41,6 @@ int main(int argc, char*argv[]){
     serverAddress.sin_port = htons(portNumber);
     serverAddress.sin_addr.s_addr = INADDR_ANY;
 
-    // //clear buffer for reuse
-    // memset(buffer, '\0', sizeof(buffer));
-
-    // printf("here11\n");
-
     if(bind(socketFD, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0){
         perror("error: bind() failed");
         exit(2);
@@ -56,47 +51,31 @@ int main(int argc, char*argv[]){
         exit(2);
     }
 
-    // establishedConnectionFD = accept(socketFD, (struct sockaddr *)&clientAddress, &sizeOfClientInfo);
-    // if(establishedConnectionFD < 0){
-    //     perror("accept() failed");
-    //     exit(1);
-    // }
-
-    // printf("here2\n");
     sizeOfClientInfo = sizeof(clientAddress);
     while(1 == 1){
-        // printf("here while start\n");
         establishedConnectionFD = accept(socketFD, (struct sockaddr *)&clientAddress, &sizeOfClientInfo);
         if(establishedConnectionFD < 0){
             perror("error: accept() failed");
         }
-        // printf("here3\n");
+
         spawnPid = fork();
-        // printf("here4\n");
         switch(spawnPid){
             case -1: //error
                 perror("error: fork()");
                 exit(1);
                 break;
             case 0: 
-                // printf("here1\n");
-
-
-
                 // zero out buffer
                 memset(buffer1, 0, sizeof(buffer1));
 
                 // receive plaintext from otp_enc
-                // printf("here5\n");
                 plaintextFileSize = read(establishedConnectionFD, buffer1, MAX_BUFFER);
                 // plaintextFileSize = lseek(plaintextFile, 0 , SEEK_END);
                 if (plaintextFileSize < 0){
                     perror("error: read _d1 failed");
-                    // printf("Error: otp_end_d could not read plaintext on port %d\n", port);
                     exit(2);
                 }
 
-                // printf("here6\n");
                 // send acknowledgement to client
                 charsWritten = write(establishedConnectionFD, "!", 1);
                 if (charsWritten < 0){
@@ -107,13 +86,11 @@ int main(int argc, char*argv[]){
                 // zero out buffer
                 memset(buffer2, 0, sizeof(buffer2));
 
-                // printf("here7\n");
                 // receive key from otp_enc
                 keyFileSize = read(establishedConnectionFD, buffer2, MAX_BUFFER);
                 // keyFileSize = lseek(keyFile, 0, SEEK_END);
                 if(keyFileSize < 0){
                     perror("error: read _d2 failed");
-                    // printf("Error: otp_end_d could not read key on port %d\n", port);
                     exit(2);
                 }
 
@@ -145,14 +122,12 @@ int main(int argc, char*argv[]){
                     // subtract 64 so that range is 0 - 26 (27 characters)
                     msgMod = msgMod - 64;
                     keyMod = keyMod - 64;
-                    // printf("%s%d%s%d\n", "msgMod: ", msgMod, ". keyMod: ", keyMod);
 
                     // combine key and message using modular addition
                     cipher = (msgMod + keyMod) % 27;
 
                     // add 64 back to that range is 64 - 90
                     cipher = cipher + 64;
-                    // printf("%s%d\n", "cipher: ", cipher);
 
                     // type conversion back to char
                     buffer3[i] = (char)cipher + 0;
@@ -170,16 +145,11 @@ int main(int argc, char*argv[]){
                     exit(2);
                 }
 
-                // printf("here case 0 end\n");
-                // exit(0);
-                // printf("here100\n");
                 break;
             default: 
-                // printf("here465\n");
+                ; //pass
                 break;
         }
-        // printf("hello world\n");
-        // exit(0);
     } // end of while
     exit(0);
 }
